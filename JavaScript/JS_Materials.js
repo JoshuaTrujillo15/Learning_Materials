89ce8ad85e49f6fb0c6a5bad1c7efe6dd5004767
Joshua Trujillo <Jtriley15@gmail.com>
2020-09-26 14:22:25 -0400

    Create JS_Materials.js

---
+++ JavaScript/JS_Materials.js
@@ -0,0 +1,22 @@
+//change content in html element id="demo"
+//JS accepts single and double quotes
+document.getElementById("demo").innerHTML = "Hello Javascript";
+
+//change img source on click
+document.getElementById('myImg').src='different.jgp';
+
+//change style of html element
+document.getElementById('demo').style.fontSize = '35px';
+
+//hide html elements
+document.getElementById('demo').style.display = 'none';
+
+//show html elements
+document.getElementById('demo').style.display = 'block';
+
+//<script> tag
+/* HTML5
+<script>
+    document.getElementById('demo').innerHTML = 'My First JavaScript';
+</script>
+*/
