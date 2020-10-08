7cfeb8d91d3058f4e8bed6178b8a1aa1986429f2
Joshua Trujillo <Jtriley15@gmail.com>
2020-10-08 11:04:52 -0400

    Update basics.php

--- PHP_Materials/basics.php
+++ PHP_Materials/basics.php
@@ -41,6 +41,158 @@ throws 'Fatal Error' on type mismatch
 
 */
 
+//assign by reference
+//ONLY named variables can be assigned by reference
+$foo = 'Bob';
+$bar = &$foo; //reference $foo via $bar
+$bar = 'My name is $bar';
+echo $bar; //returns My name is Bob
+echo $foo; //returns My name is Bob
+
+//echo vs print
+//echo marginally faster, does not set a return value
+echo 'hello';
+//sets return value 1
+print 'hello';
+$ret = print 'hello';
+print $ret; 
+//1
+
+
+//-----DATA TYPES-----//
+
+
+//var_dump() returns data value and type
+
+//string
+$str = 'hello';
+var_dump($str);
+
+//integer
+$int = 5;
+var_dump($int);
+
+//float
+$flt = 1.25;
+var_dump($flt);
+
+//boolean
+$bool = true;
+var_dump($bool);
+
+//array
+$arr = array('hello', 'i', 'am', 'an', 'array');
+var_dump($arr);
+
+//NULL
+$null = null; //also null if created without value
+var_dump($null); 
+
+//Object
+class Car {
+    public $color;
+    public $model;
+    public function __construct($color, $model) {
+        $this->color = $color;
+        $this->model = $model;
+    }
+    public function message() {
+        return 'This car is a ' . $this->color . ' ' . $this->model . '.';
+    }
+}
+
+$myCar = new Car('black', 'Volvo');
+echo $myCar -> message();
+echo '<br>';
+$myCar = new Car('red', 'Tesla');
+echo $myCar -> message();
+
+
+//-----STRING FUNCTIONS-----//
+
+$cliche = 'Hello World!'
+
+echo strlen($cliche); // 12
+echo str_word_count($cliche); // 2
+echo strrev($cliche); // !dlrow olleH
+echo strpos($cliche, 'world') // 6 [returns index at start of substring match]
+echo str_replace('world', 'friend', $cliche); // Hello friend!
+
+
+//-----NUMBERS-----//
+
+//Automatic num type conversion
+//INT size limit (2^31)-1, if exceeded, assigned as float
+$int = 29;
+$hex_int = 0xf3;
+$oct_int = 17;
+var_dump(is_int($int));
+var_dump(is_integer($hex_int)); //alias for is_int
+var_dump(is_long($oct_int)); //alias for is_int
+//all return true
+
+//NOTE
+$x = 2;
+$y = 5.5;
+$z = $x * $y; //z is float without decimal
+
+//FLOATS
+$flt = 10.75;
+$big_flt = 7.64E+5;
+var_dump(is_float($flt)); 
+var_dump(is_double($big_flt)); //alias for is_float
+
+//INFINITY
+$infinity = 1.9e411;
+var_dump(is_infinite($infinity)); //infinite check
+var_dump(is_finite($infinity)); //finite check
+
+//NaN (Not a Number) for impossible mathematical operations
+$nan = acos(8);
+var_dump(is_nan($nan));
+
+//Numeric String
+$x = 5985;
+var_dump(is_numeric($x)); //true
+
+$x = "5985";
+var_dump(is_numeric($x)); //true
+
+$x = "59.85" + 100;
+var_dump(is_numeric($x)); //true
+
+$x = "Hello";
+var_dump(is_numeric($x)); //false
+
+//Casting Strings and Floats to Integers
+$flt = 2345.678;
+$int_cast = (int)$flt;
+echo $int_cast; // 2345
+
+//-----MATHS-----//
+
+//Pi
+echo(pi()); // 3.1415926535898
+
+//min max
+$arr = (0, 150, 30, 20, -8, -200)
+echo(min($arr)); // -200
+echo(max($arr)); // 150
+
+//absolute value
+echo(abs(-6.7)); // 6.7
+
+//square root
+echo(sqrt(64)); // 8 
+
+//round
+echo(round(0.60)); // 1 [nearest int]
+
+//random
+echo(rand());
+echo(rand(10, 100)); // between 10 an 100 inclusive
+
+
 //-----SCOPE-----//
 
 //Global
@@ -91,14 +243,6 @@ myTestFourBecauseFourReallyIsNotThatBadOfANumber();
 myTestFourBecauseFourReallyIsNotThatBadOfANumber();
 myTestFourBecauseFourReallyIsNotThatBadOfANumber();
 //variable stores value even after function executed
-
-
-
-
-
-
-
-
 
 
 ?>
